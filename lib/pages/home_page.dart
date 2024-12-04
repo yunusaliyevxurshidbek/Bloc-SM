31d24c7d7b2d27fc3e078137720d235dc2f52ca3
muslmoney <ergashevbehruz.uz@gmail.com>
2024-12-04 18:25:25 +0500

    Error corrected

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,11 @@
-import 'package:bloC/bloc/home_bloc.dart';
-import 'package:bloC/bloc/home_event.dart';
-import 'package:bloC/bloc/home_state.dart';
-import 'package:bloC/services/log_service.dart';
-import 'package:bloC/views/item_of_random_user.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/home_bloc.dart';
+import '../bloc/home_event.dart';
+import '../bloc/home_state.dart';
 import '../models/random_user_list_res.dart';
+import '../services/log_service.dart';
+import '../views/item_of_random_user.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,10 +15,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   late HomeBloc homeBloc;
   ScrollController scrollController = ScrollController();
-
 
   @override
   void initState() {
@@ -31,60 +28,57 @@ class _HomePageState extends State<HomePage> {
       if (scrollController.position.maxScrollExtent <= scrollController.offset) {
         LogService.i(homeBloc.currentPage.toString());
         homeBloc.add(LoadRandomUserListEvent());
-
-
       }
     });
-
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromRGBO(232, 232, 232, 1),
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text("Random User - Bloc"),
-        ),
-        body:BlocBuilder<HomeBloc,HomeState>(
-    builder: (context,state){
-      if (state is HomeErrorState){
-        return viewOfError(state.errorMessage);
-      }
+      backgroundColor: const Color.fromRGBO(232, 232, 232, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("Random User - BloC(Bloc)"),
+      ),
+      body: BlocBuilder<HomeBloc, HomeState>(
+        buildWhen: (previous, current){
+          return current is HomeRandomUserListState;
+        },
+        builder: (context, state) {
+          if (state is HomeErrorState) {
+            return viewOfError(state.errorMessage);
+          }
 
-      if (state is HomeRandomUserListState){
-        var userList = state.userList;
-        return viewOfRandomUserList(userList.cast<RandomUser>());
-      }
+          if (state is HomeRandomUserListState) {
+            var userList = state.userList;
+            return viewOfRandomUserList(userList);
+          }
 
-      return viewOfLoading;
-
-    }
-        ),
+          return viewOfLoading();
+        },
+      ),
     );
   }
 
-  Widget viewOfLoading(){
+  Widget viewOfError(String err) {
     return Center(
+      child: Text("Error occurred $err"),
+    );
+  }
+
+  Widget viewOfLoading() {
+    return const Center(
       child: CircularProgressIndicator(),
     );
   }
 
-  Widget viewOfError(String err){
-    return Center(
-      child: Text(err),
-    );
-  }
-
-  Widget viewOfRandomUserList(List<RandomUser> userList){
+  Widget viewOfRandomUserList(List<RandomUser> userList) {
     return ListView.builder(
       controller: scrollController,
       itemCount: userList.length,
-      itemBuilder: (ctx,index){
-        return itemOfRandomUser(userList[index],index);
-      }
+      itemBuilder: (ctx, index) {
+        return itemOfRandomUser(userList[index], index);
+      },
     );
   }
-
 }
